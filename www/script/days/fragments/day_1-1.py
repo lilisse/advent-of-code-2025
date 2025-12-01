c92986e9a59aefdbb855ae3ece6af9a6ba1184f2
ulysse chosson <u.chosson@proton.me>
2025-12-01 12:49:54 +0100

    day 1 solved :party:

--- www/script/days/fragments/day_1-1.py
+++ www/script/days/fragments/day_1-1.py
@@ -1,3 +1,18 @@
-def solve(data: list[str]) -> str:
-    # this is the day 1 - 1
-    return " ".join(data)
+def get_ope_list(parsed_input: list[str]) -> list[int]:
+    result = []
+    for element in parsed_input:
+        if element.startswith("R"):
+            result.append(int(element[1:]))
+        elif element.startswith("L"):
+            result.append(-int(element[1:]))
+    return result
+
+def solve(parsed_input: list[str]) -> int:
+    ope_list = get_ope_list(parsed_input)
+    result = 0
+    pointer = 50
+
+    for ope in ope_list:
+        result += (pointer := (pointer + ope) % 100) == 0
+
+    return result
--- www/script/utils/utils.py
+++ www/script/utils/utils.py
@@ -3,13 +3,20 @@ from json import load
 from pyscript import display
 
 
+def get_content_of_input(file_path: str) -> str:
+    with open(file_path, "r") as input_file:
+        return input_file.read()
+
 def parse_input(content: str) -> list[str]:
-    content_by_line = content.split("\n")
-    return [str(len(content)) for content in content_by_line]
+    return content.split("\n")
 
 def display_a_file(file_name: str, target: str) -> None:
     with open(file_name, "r") as subject:
-        display(subject.read(), target=target)
+        if file_name.endswith(".txt"):
+            for line in parse_input(subject.read()):
+                display(line, target=target, append=True)
+        else:
+            display(subject.read(), target=target)
 
 def display_day_name(day_number: str, target: str) -> None:
     with open("./names.json", "r") as names_files:
