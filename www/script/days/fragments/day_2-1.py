67e99622d69a86719fe2526855a12cdd6b7438b4
ulysse chosson <u.chosson@proton.me>
2025-12-02 16:36:05 +0100

    Day 2 solved

--- www/script/days/fragments/day_2-1.py
+++ www/script/days/fragments/day_2-1.py
@@ -1,3 +1,21 @@
-def solve(data: list[str]) -> str:
-    # this is the day 2 - 1
-    return " ".join(data)
+def get_product_ids(parsed_input: list[str]) -> list[str]:
+    return [
+        str(num)
+        for item in parsed_input
+        for start, end in [map(int, item.split("-"))]
+        for num in range(start, end + 1)
+    ]
+
+#  Raccourcire
+def solve(parsed_input: list[str]) -> int:
+    product_ids = get_product_ids(parsed_input)
+    result = 0
+
+    for product_id in product_ids:
+        if (
+            len(product_id) % 2 == 0 and
+            product_id[:len(product_id)//2] == product_id[len(product_id)//2:]
+        ):
+            result += int(product_id)
+
+    return result
--- www/script/days/fragments/day_2-2.py
+++ www/script/days/fragments/day_2-2.py
@@ -1,3 +1,23 @@
-def solve(data: list[str]) -> str:
-    # this is the day 2 - 2
-    return " ".join(data)
+def get_product_ids(parsed_input: list[str]) -> list[str]:
+    return [
+        str(num)
+        for item in parsed_input
+        for start, end in [map(int, item.split("-"))]
+        for num in range(start, end + 1)
+    ]
+
+def solve(parsed_input: list[str]) -> int:
+    product_ids = get_product_ids(parsed_input)
+    result = 0
+
+    for pid in product_ids:
+        len_pid = len(pid)
+        for pattern_len in range(1, (len_pid // 2) + 1):
+            if (
+                len_pid % pattern_len == 0
+                and pid[:pattern_len] * (len_pid // pattern_len) == pid
+            ):
+                result += int(pid)
+                break
+
+    return result
